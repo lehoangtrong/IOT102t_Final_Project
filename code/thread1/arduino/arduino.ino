4d19030973b6891c62a46471d9944a29fd34987e
LeHoangTrong <lehoangtrongcsgl@gmail.com>
2024-06-27 17:52:40 +0700

    change read fingerprint is ESP8266

--- code/thread1/arduino/arduino.ino
+++ code/thread1/arduino/arduino.ino
@@ -1,117 +1,258 @@
 #include <Adafruit_Fingerprint.h>
 #include <SoftwareSerial.h>
-#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "defind.h"
+// #include <LiquidCrystal_I2C.h>
 
 #define PIN_FIRE_SENSOR 6
 #define PIN_PIR_SENSOR 7
 #define PIN_AIR_SENSOR A0
-int fireSensorValue = 0;
-int pirSensorValue = 0;
-int airSensorValue = 0;
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-SoftwareSerial mySerial(2, 3);
+uint8_t fireSensorValue = 0;
+uint8_t pirSensorValue = 0;
+uint8_t airSensorValue = 0;
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(4);
+
+// LiquidCrystal_I2C lcd(0x27, 16, 2);
+SoftwareSerial mySerial(4, 5);
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
-Adafruit_SSD1306 display; // Create display
 
-void cleanSerialBuffer(); // Xóa bộ đệm của Serial
-bool checkSensorFire();   // Kiểm tra cảm biến cháy
+void displayOled(uint8_t choice);
 
-void setup()
-{
-    lcd.init();
-    lcd.backlight();
-    lcd.setCursor(0, 0);
-    lcd.print("Hello, world!");
-    lcd.setCursor(0, 1);
-    lcd.print("Hello, world!");
+bool checkSensorFire();  // Kiểm tra cảm biến cháy
+void fingerSystem();     // Hệ thống vân tay
 
-    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
-    { // Address 0x3D for 128x64
-        Serial.println(F("SSD1306 allocation failed"));
-        for (;;)
-            ;
-    }
+void setup() {
+  // lcd.init();
+  // lcd.backlight();
 
-    Serial.begin(9600);
-    while (!Serial)
-        ;
-    Serial.println("Adafruit finger detect test");
+  pinMode(PIN_FIRE_SENSOR, INPUT);
+  pinMode(PIN_PIR_SENSOR, INPUT);
+  pinMode(PIN_AIR_SENSOR, INPUT);
 
-    finger.begin(57600);
-    if (finger.verifyPassword())
-    {
-        Serial.println("Found fingerprint sensor!");
-    }
-    else
-    {
-        Serial.println("Did not find fingerprint sensor :(");
-        while (1)
-            ;
-    }
+  Serial.begin(115200);
 
-    finger.getTemplateCount();
-    Serial.print("Sensor contains ");
-    Serial.print(finger.templateCount);
-    Serial.println(" templates");
-    Serial.println("Waiting for valid finger...");
+  // text display tests
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;
+  }
+
+  Serial.println("Adafruit finger detect test");
+
+  finger.begin(57600);
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1)
+      ;
+  }
+
+  finger.getTemplateCount();
+  Serial.print("Sensor contains ");
+  Serial.print(finger.templateCount);
+  Serial.println(" templates");
+  Serial.println("Waiting for valid finger...");
+
+  display.clearDisplay();
 }
 
-void loop()
-{
-    // Read sensor values
-    fireSensorValue = digitalRead(PIN_FIRE_SENSOR);
-    pirSensorValue = digitalRead(PIN_PIR_SENSOR);
-    airSensorValue = analogRead(PIN_AIR_SENSOR);
+void loop() {
+  // Read sensor values
+  fireSensorValue = digitalRead(PIN_FIRE_SENSOR);
+  pirSensorValue = digitalRead(PIN_PIR_SENSOR);
+  airSensorValue = analogRead(PIN_AIR_SENSOR);
 
+  if (!checkSensorValue()) {
+    fingerSystem();
+  } else {
+    Serial.println("System is safe!!!!!");
     display.clearDisplay();
-    display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
     display.setCursor(0, 0);
+    display.println("System is safe");
     display.println("Fire sensor: " + String(fireSensorValue));
     display.println("PIR sensor: " + String(pirSensorValue));
     display.println("Air sensor: " + String(airSensorValue));
     display.display();
-
-    Serial.println("PIR sensor: " + String(pirSensorValue));
-    // Serial.println("=====================================");
-    // Serial.println("Fingerprint system");
-    // Serial.println("Choose a number from the following list:");
-    // Serial.println("1. Enroll a new fingerprint");
-    // Serial.println("2. Update a fingerprint");
-    // Serial.println("3. Delete a fingerprint");
-
-    // int choice = 0;
-    // while (Serial.available() == 0)
-    //     ;
-    // choice = Serial.parseInt();
-    // clearSerialBuffer();
-
-    // switch (choice)
-    // {
-    // case 1:
-    //     Serial.println("Enroll a new fingerprint");
-    //     break;
-    // case 2:
-    //     Serial.println("Update a fingerprint");
-    //     break;
-    // case 3:
-    //     Serial.println("Delete a fingerprint");
-    //     break;
-    // default:
-    //     Serial.println("Invalid choice");
-    //     break;
-    // }
-
-    // delay(50);
+    // Alarm system here
+  }
 }
 
-void clearSerialBuffer()
-{
-    while (Serial.available() > 0)
-    {
-        char t = Serial.read();
+bool checkSensorValue() {
+  bool check = false;
+  if (fireSensorValue == 1 || airSensorValue > 400) {
+    if (pirSensorValue == 1) {
+      check = true;
+    } else {
+      check = false;
     }
+  }
+  return check;
+}
+
+void fingerSystem() {
+  // get finger ID
+  uint8_t p = finger.getImage();
+
+  displayOled(4);
+
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.print(".");
+      return;
+    default:
+      Serial.println("Unknown error");
+      return;
+  }
+
+  // OK success!
+  displayOled(6);
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return;
+    default:
+      Serial.println("Unknown error");
+      return;
+  }
+
+  // OK converted!
+  p = finger.fingerFastSearch();
+
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+    // found a match!
+    Serial.print("Found ID #");
+    Serial.print(finger.fingerID);
+    Serial.print(" with confidence of ");
+    Serial.println(finger.confidence);
+
+    displayOled(7);
+
+    // show id fingerprint
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    display.println("Finger ID: " + String(finger.fingerID));
+    display.display();
+
+    // Wait esp8266 uart read fingerprint id and check in database
+
+    delay(1000);
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    displayOled(5);
+    Serial.println("Did not find a match");
+    delay(1000);
+    return;
+  } else {
+    Serial.println("Unknown error");
+    return;
+  }
+
+  // delay(1000);
+
+  // Check if the ID is in the database
+  // for (int i = 0; i < 3; i++)
+  // {
+  //     if (finger.fingerID == students[i].studentFingerID)
+  //     {
+  //         Serial.println("Welcome " + students[i].studentName);
+  //         break;
+  //     }
+  // }
+  // delay(1000);
+}
+
+void displayOled(uint8_t choice) {
+  if (choice == 1) {
+    String ssid = "FPT_Students";
+    // show username of wifi
+    display.clearDisplay();
+    display.setTextSize(1);       // Normal 1:1 pixel scale
+    display.setTextColor(WHITE);  // Draw white text
+    display.setCursor(0, 0);      // Start at top-left corner
+    display.print(F("Connecting to \n"));
+    display.setCursor(0, 50);
+    display.setTextSize(2);
+    display.print(ssid);
+    display.drawBitmap(73, 10, Wifi_start_bits, Wifi_start_width, Wifi_start_height, WHITE);
+    display.display();
+    delay(1000);
+    return;
+  }
+  if (choice == 2) {
+    display.clearDisplay();
+    display.setTextSize(2);       // Normal 1:1 pixel scale
+    display.setTextColor(WHITE);  // Draw white text
+    display.setCursor(8, 0);      // Start at top-left corner
+    display.print(F("Connected \n"));
+    display.drawBitmap(33, 15, Wifi_connected_bits, Wifi_connected_width, Wifi_connected_height, WHITE);
+    display.display();
+    delay(1000);
+    return;
+  }
+  if (choice == 3) {
+    display.clearDisplay();
+    display.drawBitmap(32, 0, FinPr_failed_bits, FinPr_failed_width, FinPr_failed_height, WHITE);
+    display.display();
+    delay(1000);
+    return;
+  }
+
+  if (choice == 4) {
+    display.clearDisplay();
+    display.drawBitmap(32, 0, FinPr_scan_bits, FinPr_scan_width, FinPr_scan_height, WHITE);
+    display.display();
+    delay(1000);
+    return;
+  }
+  if (choice == 6) {
+    display.clearDisplay();
+    display.drawBitmap(32, 0, FinPr_start_bits, FinPr_start_width, FinPr_start_height, WHITE);
+    display.display();
+    delay(1000);
+    return;
+  }
+
+  if (choice == 7) {
+    display.clearDisplay();
+    display.drawBitmap(32, 0, FinPr_valid_bits, FinPr_valid_width, FinPr_valid_height, WHITE);
+    display.display();
+    delay(1000);
+    return;
+  }
+
+  if (choice == 8) {
+    display.clearDisplay();
+    display.drawBitmap(32, 0, FinPr_invalid_bits, FinPr_invalid_width, FinPr_invalid_height, WHITE);
+    display.display();
+    delay(1000);
+    return;
+  }
 }
--- code/thread1/esp8266/esp8266.ino
+++ code/thread1/esp8266/esp8266.ino
@@ -1,2 +1,253 @@
-#include<ESP8266WiFi.h>
+#include <ESP8266WiFi.h>
+// Finger print library
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+// OLED display library
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "defind.h"
 
+// Wifi library
+#include <WiFiClientSecure.h>
+
+const char *host = "script.google.com";
+const int httpsPort = 443;
+
+// Fingerprint scanner Pins
+#define Finger_Rx 14  // D5
+#define Finger_Tx 12  // D6
+// Declaration for SSD1306 display connected using software I2C
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+#define OLED_RESET 0      // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+SoftwareSerial mySerial(Finger_Rx, Finger_Tx);
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+const char *ssid = "FPT_Error";
+const char *password = "nonepassword";
+
+WiFiClientSecure client;
+
+void fingerSystem();
+void displayOled(uint8_t choice);
+
+void getName(String fingerID);
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // text display tests
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;
+  }
+
+  Serial.println("Adafruit finger detect test");
+
+  finger.begin(57600);
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1)
+      ;
+  }
+
+  finger.getTemplateCount();
+  Serial.print("Sensor contains ");
+  Serial.print(finger.templateCount);
+  Serial.println(" templates");
+  Serial.println("Waiting for valid finger...");
+
+  display.clearDisplay();
+
+  client.setInsecure();
+}
+
+void loop() {
+  fingerSystem();
+}
+
+void fingerSystem() {
+  // get finger ID
+  uint8_t p = finger.getImage();
+
+  displayOled(4);
+
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.print(".");
+      return;
+    default:
+      Serial.println("Unknown error");
+      return;
+  }
+
+  // OK success!
+  displayOled(6);
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return;
+    default:
+      Serial.println("Unknown error");
+      return;
+  }
+
+  // OK converted!
+  p = finger.fingerFastSearch();
+
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+    // found a match!
+    Serial.print("Found ID #");
+    Serial.print(finger.fingerID);
+    Serial.print(" with confidence of ");
+    Serial.println(finger.confidence);
+
+    displayOled(7);
+
+    // show id fingerprint
+
+    if (checkStudent(finger.fingerID)) {
+      getName(finger.fingerID);
+      display.clearDisplay();
+      display.setTextSize(1);
+      display.setTextColor(SSD1306_WHITE);
+      display.setCursor(0, 0);
+      display.println("Finger ID: " + String(finger.fingerID));
+      display.display();
+    }
+    delay(1000);
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    displayOled(5);
+    Serial.println("Did not find a match");
+    delay(1000);
+    return;
+  } else {
+    Serial.println("Unknown error");
+    return;
+  }
+}
+
+void displayOled(uint8_t choice) {
+  if (choice == 1) {
+    String ssid = "FPT_Students";
+    // show username of wifi
+    display.clearDisplay();
+    display.setTextSize(1);       // Normal 1:1 pixel scale
+    display.setTextColor(WHITE);  // Draw white text
+    display.setCursor(0, 0);      // Start at top-left corner
+    display.print(F("Connecting to \n"));
+    display.setCursor(0, 50);
+    display.setTextSize(2);
+    display.print(ssid);
+    display.drawBitmap(73, 10, Wifi_start_bits, Wifi_start_width, Wifi_start_height, WHITE);
+    display.display();
+    delay(1000);
+    return;
+  }
+  if (choice == 2) {
+    display.clearDisplay();
+    display.setTextSize(2);       // Normal 1:1 pixel scale
+    display.setTextColor(WHITE);  // Draw white text
+    display.setCursor(8, 0);      // Start at top-left corner
+    display.print(F("Connected \n"));
+    display.drawBitmap(33, 15, Wifi_connected_bits, Wifi_connected_width, Wifi_connected_height, WHITE);
+    display.display();
+    delay(1000);
+    return;
+  }
+  if (choice == 3) {
+    display.clearDisplay();
+    display.drawBitmap(32, 0, FinPr_failed_bits, FinPr_failed_width, FinPr_failed_height, WHITE);
+    display.display();
+    delay(1000);
+    return;
+  }
+
+  if (choice == 4) {
+    display.clearDisplay();
+    display.drawBitmap(32, 0, FinPr_scan_bits, FinPr_scan_width, FinPr_scan_height, WHITE);
+    display.display();
+    delay(1000);
+    return;
+  }
+  if (choice == 6) {
+    display.clearDisplay();
+    display.drawBitmap(32, 0, FinPr_start_bits, FinPr_start_width, FinPr_start_height, WHITE);
+    display.display();
+    delay(1000);
+    return;
+  }
+
+  if (choice == 7) {
+    display.clearDisplay();
+    display.drawBitmap(32, 0, FinPr_valid_bits, FinPr_valid_width, FinPr_valid_height, WHITE);
+    display.display();
+    delay(1000);
+    return;
+  }
+
+  if (choice == 8) {
+    display.clearDisplay();
+    display.drawBitmap(32, 0, FinPr_invalid_bits, FinPr_invalid_width, FinPr_invalid_height, WHITE);
+    display.display();
+    delay(1000);
+    return;
+  }
+}
+
+void getName(String fingerID) {
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  String url = "/macros/s/" + GAS_ID + "/exec?id=" + id;
+  Serial.println(url);
+
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "User-Agent: BuildFailureDetectorESP8266\r\n" + "Connection: close\r\n\r\n");
+  Serial.println("OK");
+}
