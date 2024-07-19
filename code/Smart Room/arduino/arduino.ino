78f7a507bffd49ad586f6742b02184bff5489ff8
LeHoangTrong <lehoangtrongcsgl@gmail.com>
2024-07-19 21:03:06 +0700

    fix arduino and add sensor ultrasonic sensor

--- code/Smart Room/arduino/arduino.ino
+++ code/Smart Room/arduino/arduino.ino
@@ -6,10 +6,14 @@
 #define PIN_PIR_SENSOR 7
 #define PIN_AIR_SENSOR A0
 #define PIN_SERVO_MOTOR 9
+#define PIN_BUZZER 8
 
 uint8_t fireSensorValue = 0;
 uint8_t pirSensorValue = 0;
 uint8_t airSensorValue = 0;
+const unsigned int TRIG_PIN = 13;
+const unsigned int ECHO_PIN = 12;
+int duration, distance;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 Servo servoMotor;
@@ -23,22 +27,18 @@ void setup()
   lcd.init();
   lcd.backlight();
   lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Hello");
 
   pinMode(PIN_FIRE_SENSOR, INPUT);
   pinMode(PIN_PIR_SENSOR, INPUT);
   pinMode(PIN_AIR_SENSOR, INPUT);
+  pinMode(PIN_BUZZER, OUTPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
   servoMotor.attach(PIN_SERVO_MOTOR);
 }
 
 void loop()
 {
-  // Read sensor values
-  fireSensorValue = analogRead(PIN_FIRE_SENSOR);
-  pirSensorValue = digitalRead(PIN_PIR_SENSOR);
-  airSensorValue = analogRead(PIN_AIR_SENSOR);
-
   if (checkSensorValue())
   {
     Serial.println("System is not safe!!!!!");
@@ -60,9 +60,19 @@ void loop()
     lcd.print(String(airSensorValue));
     lcd.setCursor(14, 1);
     lcd.print(String(pirSensorValue));
-    // Open door
-    servoMotor.write(90);
-    delay(3000);
+
+    // Warning
+    while (checkSensorValue())
+    {
+      tone(PIN_BUZZER, 1000);
+      delay(500);
+      noTone(PIN_BUZZER);
+
+      // Open door
+      servoMotor.write(0);
+    }
+
+    delay(4000);
   }
   else
   {
@@ -87,7 +97,7 @@ void loop()
     lcd.print(String(pirSensorValue));
 
     // Close door
-    servoMotor.write(0);
+    servoMotor.write(150);
   }
 
   if (Serial.available())
@@ -96,28 +106,43 @@ void loop()
     Serial.println(data);
     if (data.indexOf("OPEN") >= 0)
     {
-      servoMotor.write(90);
+      servoMotor.write(0);
       delay(3000); // maybe can change read sensor value to check door is open or close
 
-      servoMotor.write(0);
+      servoMotor.write(150);
     }
   }
-  delay(2000);
+
+  if (distance < 6)
+  {
+    Serial.println("Door is opening");
+    servoMotor.write(0);
+    delay(2000);
+    servoMotor.write(150);
+  }
+
+  delay(1000);
 }
 
 bool checkSensorValue()
 {
+  // Read sensor values
+  fireSensorValue = analogRead(PIN_FIRE_SENSOR);
+  pirSensorValue = digitalRead(PIN_PIR_SENSOR);
+  airSensorValue = analogRead(PIN_AIR_SENSOR);
+
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  duration = pulseIn(ECHO_PIN, HIGH);
+  distance = (duration / 2) / 29.1;
+
   bool check = false;
   if (fireSensorValue < 200 || airSensorValue > 400 || airSensorValue < 40)
   {
-    if (pirSensorValue == 1)
-    {
-      check = true;
-    }
-    else
-    {
-      check = false;
-    }
+    check = true;
   }
   return check;
 }
--- code/Smart Room/esp8266/esp8266.ino
+++ code/Smart Room/esp8266/esp8266.ino
@@ -18,12 +18,12 @@ const int httpsPort = 443;
 String GAS_ID = "AKfycbwrPlg8llnoHhIu_0dLCJ6fINCWptultwJGdIlPefFAJxLHR-EpWe-NsNVuNgdRNNdOXQ";
 
 // Fingerprint scanner Pins
-#define Finger_Rx 14  // D5
-#define Finger_Tx 12  // D6
+#define Finger_Rx 14 // D5
+#define Finger_Tx 12 // D6
 // Declaration for SSD1306 display connected using software I2C
-#define SCREEN_WIDTH 128  // OLED display width, in pixels
-#define SCREEN_HEIGHT 64  // OLED display height, in pixels
-#define OLED_RESET 0      // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET 0     // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 SoftwareSerial mySerial(Finger_Rx, Finger_Tx);
@@ -36,16 +36,18 @@ String getStudentByFingerID = String("/macros/s/") + GAS_ID + "/exec?type=getStu
 
 HTTPSRedirect *client = nullptr;
 
-void fingerSystem();                 // take attendance by fingerprint
-void displayOled(uint8_t choice);    // display oled
-String checkStudent(int fingerID);   // check student by fingerID
-void connectToGoogleSheet();         // connect to google sheet
-String sendGoogleSheet(String url);  // send data to google sheet
+void fingerSystem();                // take attendance by fingerprint
+void displayOled(uint8_t choice);   // display oled
+String checkStudent(int fingerID);  // check student by fingerID
+void connectToGoogleSheet();        // connect to google sheet
+String sendGoogleSheet(String url); // send data to google sheet
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   delay(10);
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
     Serial.println(F("SSD1306 allocation failed"));
     for (;;)
       ;
@@ -54,9 +56,12 @@ void setup() {
 
   Serial.println("Adafruit finger detect test");
   finger.begin(57600);
-  if (finger.verifyPassword()) {
+  if (finger.verifyPassword())
+  {
     Serial.println("Found fingerprint sensor!");
-  } else {
+  }
+  else
+  {
     Serial.println("Did not find fingerprint sensor :(");
     for (;;)
       ;
@@ -69,7 +74,8 @@ void setup() {
 
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     displayOled(wifi_connect_icon);
     delay(500);
     Serial.print(".");
@@ -83,61 +89,66 @@ void setup() {
   delay(1000);
 }
 
-void loop() {
-  fingerSystem();  // take attendance by fingerprint
+void loop()
+{
+  fingerSystem(); // take attendance by fingerprint
   yield();
 }
 
-void fingerSystem() {
+void fingerSystem()
+{
   uint8_t p = finger.getImage();
 
   displayOled(fingerprint_scan_icon);
 
-  switch (p) {
-    case FINGERPRINT_OK:
-      Serial.println("Image taken");
-      break;
-    case FINGERPRINT_NOFINGER:
-      Serial.print(".");
-      return;
-    default:
-      Serial.println("Unknown error");
-      return;
+  switch (p)
+  {
+  case FINGERPRINT_OK:
+    Serial.println("Image taken");
+    break;
+  case FINGERPRINT_NOFINGER:
+    Serial.print(".");
+    return;
+  default:
+    Serial.println("Unknown error");
+    return;
   }
 
   // OK success!
   displayOled(fingerprint_icon);
 
   p = finger.image2Tz();
-  switch (p) {
-    case FINGERPRINT_OK:
-      Serial.println("Image converted");
-      break;
-    case FINGERPRINT_IMAGEMESS:
-      Serial.println("Image too messy");
-      return;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      return;
-    case FINGERPRINT_FEATUREFAIL:
-      Serial.println("Could not find fingerprint features");
-      return;
-    case FINGERPRINT_INVALIDIMAGE:
-      Serial.println("Could not find fingerprint features");
-      return;
-    default:
-      Serial.println("Unknown error");
-      return;
+  switch (p)
+  {
+  case FINGERPRINT_OK:
+    Serial.println("Image converted");
+    break;
+  case FINGERPRINT_IMAGEMESS:
+    Serial.println("Image too messy");
+    return;
+  case FINGERPRINT_PACKETRECIEVEERR:
+    Serial.println("Communication error");
+    return;
+  case FINGERPRINT_FEATUREFAIL:
+    Serial.println("Could not find fingerprint features");
+    return;
+  case FINGERPRINT_INVALIDIMAGE:
+    Serial.println("Could not find fingerprint features");
+    return;
+  default:
+    Serial.println("Unknown error");
+    return;
   }
 
   // OK converted!
   p = finger.fingerFastSearch();
 
-  if (p == FINGERPRINT_OK) {
+  if (p == FINGERPRINT_OK)
+  {
 
     Serial.print("  OPEN\r\n");
     delay(500);
-    Serial.print("  OPEN\r\n");
+    // Serial.print("  OPEN\r\n");
     Serial.println("Found a print match!");
     // found a match!
     Serial.print("Found ID #");
@@ -148,7 +159,8 @@ void fingerSystem() {
     displayOled(fingerprint_valid_icon);
     String response = checkStudent(finger.fingerID);
 
-    if (response.indexOf("studentID") != -1) {
+    if (response.indexOf("studentID") != -1)
+    {
       Serial.println("Attendant taken");
       String student = response;
       // {"studentID":"QE180205","studentName":"Vũ Thị Kim Liên","fingerID":21}
@@ -164,31 +176,38 @@ void fingerSystem() {
       display.setCursor(0, 20);
       display.print(F("Student Name: "));
       display.setCursor(0, 30);
-      display.print(studentName);  // TODO: Encode UTF-8 not working
+      display.print(studentName); // TODO: Encode UTF-8 not working
       display.display();
-
-    } else {
+    }
+    else
+    {
       Serial.println("Attendant not taken");
     }
     delay(1000);
-  } else if (p == FINGERPRINT_NOTFOUND) {
+  }
+  else if (p == FINGERPRINT_NOTFOUND)
+  {
     displayOled(fingerprint_invalid_icon);
     Serial.println("Did not find a match");
     delay(1000);
     return;
-  } else {
+  }
+  else
+  {
     Serial.println("Unknown error");
     return;
   }
 }
 
-void displayOled(uint8_t choice) {
-  if (choice == 1) {
+void displayOled(uint8_t choice)
+{
+  if (choice == 1)
+  {
     // show username of wifi
     display.clearDisplay();
-    display.setTextSize(1);       // Normal 1:1 pixel scale
-    display.setTextColor(WHITE);  // Draw white text
-    display.setCursor(0, 0);      // Start at top-left corner
+    display.setTextSize(1);      // Normal 1:1 pixel scale
+    display.setTextColor(WHITE); // Draw white text
+    display.setCursor(0, 0);     // Start at top-left corner
     display.print(F("Connecting to \n"));
     display.setCursor(0, 50);
     display.setTextSize(2);
@@ -198,18 +217,20 @@ void displayOled(uint8_t choice) {
     delay(1000);
     return;
   }
-  if (choice == 2) {
+  if (choice == 2)
+  {
     display.clearDisplay();
-    display.setTextSize(2);       // Normal 1:1 pixel scale
-    display.setTextColor(WHITE);  // Draw white text
-    display.setCursor(8, 0);      // Start at top-left corner
+    display.setTextSize(2);      // Normal 1:1 pixel scale
+    display.setTextColor(WHITE); // Draw white text
+    display.setCursor(8, 0);     // Start at top-left corner
     display.print(F("Connected \n"));
     display.drawBitmap(33, 15, Wifi_connected_bits, Wifi_connected_width, Wifi_connected_height, WHITE);
     display.display();
     delay(1000);
     return;
   }
-  if (choice == 3) {
+  if (choice == 3)
+  {
     display.clearDisplay();
     display.drawBitmap(32, 0, FinPr_failed_bits, FinPr_failed_width, FinPr_failed_height, WHITE);
     display.display();
@@ -217,14 +238,16 @@ void displayOled(uint8_t choice) {
     return;
   }
 
-  if (choice == 4) {
+  if (choice == 4)
+  {
     display.clearDisplay();
     display.drawBitmap(32, 0, FinPr_scan_bits, FinPr_scan_width, FinPr_scan_height, WHITE);
     display.display();
     delay(1000);
     return;
   }
-  if (choice == 5) {
+  if (choice == 5)
+  {
     display.clearDisplay();
     display.drawBitmap(32, 0, FinPr_start_bits, FinPr_start_width, FinPr_start_height, WHITE);
     display.display();
@@ -232,7 +255,8 @@ void displayOled(uint8_t choice) {
     return;
   }
 
-  if (choice == 6) {
+  if (choice == 6)
+  {
     display.clearDisplay();
     display.drawBitmap(32, 0, FinPr_valid_bits, FinPr_valid_width, FinPr_valid_height, WHITE);
     display.display();
@@ -240,7 +264,8 @@ void displayOled(uint8_t choice) {
     return;
   }
 
-  if (choice == 7) {
+  if (choice == 7)
+  {
     display.clearDisplay();
     display.drawBitmap(32, 0, FinPr_invalid_bits, FinPr_invalid_width, FinPr_invalid_height, WHITE);
     display.display();
@@ -249,46 +274,60 @@ void displayOled(uint8_t choice) {
   }
 }
 
-String checkStudent(int fingerID) {
+String checkStudent(int fingerID)
+{
   bool check = false;
   // Serial.print("connecting to ");
   // Serial.println(host);
   String url = takeAttendant + "fingerID=" + fingerID;
   String response = sendGoogleSheet(url);
-  if (response.indexOf("studentID") != -1) {
+  if (response.indexOf("studentID") != -1)
+  {
     check = true;
-  } else if (response.indexOf("FingerID not found") != -1) {
+  }
+  else if (response.indexOf("FingerID not found") != -1)
+  {
     check = false;
-  } else {
+  }
+  else
+  {
     checkStudent(fingerID);
   }
 
   return check ? response : "";
 }
 
-String sendGoogleSheet(String url) {
+String sendGoogleSheet(String url)
+{
   connectToGoogleSheet();
   String respone = "";
-  if (client->GET(url, host)) {
+  if (client->GET(url, host))
+  {
     // Serial.println("Yêu cầu GET thành công");
     // Serial.println("Respone:");
     // Serial.println(client->getResponseBody());
     respone = client->getResponseBody();
-  } else {
+  }
+  else
+  {
     // Serial.println("Yêu cầu GET thất bại");
   }
   return respone;
 }
 
-void connectToGoogleSheet() {
-  if (client == nullptr) {
+void connectToGoogleSheet()
+{
+  if (client == nullptr)
+  {
     client = new HTTPSRedirect(httpsPort);
     client->setInsecure();
 
     client->setPrintResponseBody(false);
     client->setContentTypeHeader("application/json");
   }
-  if (!client->connected()) {
+  if (!client->connected())
+  {
     client->connect(host, httpsPort);
   }
 }
+  
